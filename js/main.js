c66e5a7f237fec8d092f2c73a4540ab5709c1a03
KarynaKovhan <karinakovgan3006@gmail.com>
2022-06-29 02:14:34 +0300

    added select and checkboxes

--- js/main.js
+++ js/main.js
@@ -1,30 +1,38 @@
-let select = function () {
-    let selectHeader = document.querySelectorAll('.select__header');
-    let selectItem = document.querySelectorAll('.select__item');
-  
-    selectHeader.forEach(item => {
-        item.addEventListener('click', selectToggle)
-    });
-  
-    selectItem.forEach(item => {
-        item.addEventListener('click', selectChoose)
-    });
-  
-    function selectToggle() {
-        this.parentElement.classList.toggle('is-active');
-    }
-  
-    function selectChoose() {
-        let text = this.innerText,
-            select = this.closest('.select'),
-            currentText = select.querySelector('.select__current');
-        currentText.innerText = text;
-        select.classList.remove('is-active');
-  
-    }
-  
-  };
-  
-  
-  select();
+const selected = document.querySelector(".selected");
+const optionsContainer = document.querySelector(".options-container");
+const searchBox = document.querySelector(".search-box input");
 
+const optionsList = document.querySelectorAll(".option");
+
+selected.addEventListener("click", () => {
+  optionsContainer.classList.toggle("active");
+
+  searchBox.value = "";
+  filterList("");
+
+  if (optionsContainer.classList.contains("active")) {
+    searchBox.focus();
+  }
+});
+
+optionsList.forEach(o => {
+  o.addEventListener("click", () => {
+    selected.innerHTML = o.querySelector("label").innerHTML;
+    optionsContainer.classList.remove("active");
+  });
+});
+
+searchBox.addEventListener("keyup", function(e) {
+  filterList(e.target.value);
+});
+
+const filterList = searchTerm => {
+  searchTerm = searchTerm.toLowerCase();
+  optionsList.forEach(option => {
+    if (label.indexOf(searchTerm) != -1) {
+      option.style.display = "block";
+    } else {
+      option.style.display = "none";
+    }
+  });
+};
